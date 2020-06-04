2b763c8e5f54bb9f0350a9a8ba5ad41ae80a330d
Ben Parish <parishben13@gmail.com>
2020-06-04 03:40:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+require './lib/user.rb'
+class Student < User
+attr_accessor :first_name, :last_name
+def initialize
+  @knowledge = []
+end
+def knowledge
+  @knowledge
+end
+def learn
 end
